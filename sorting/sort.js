9f3e2ecd899f1b9d7b9ecff6e65cfc17b5fa8052
arnihere <arnur.makenov@gmail.com>
2020-07-16 02:25:55 +0900

    added animation so that colored guys get outta grid smoothly

--- sorting/sort.js
+++ sorting/sort.js
@@ -1,7 +1,15 @@
+var Color = net.brehaut.Color
+
+
 Vue.component("cell-component", {
-    props: ['position', 'dragging', 'x', 'y', 'hcells', 'vcells'],
+    props: ['position', 'dragging', 'x', 'y', 'hcells', 'vcells', 'disappear'],
     data: function(){
-        return {color : null}
+        return {color : null, tweenedColor: {
+            red: 0,
+            green: 0,
+            blue: 0,
+            alpha: 1
+        }}
     },
     computed: {
         cellSize: function(){
@@ -12,37 +20,65 @@ Vue.component("cell-component", {
         },
         posY: function(){
             return this.cellSize * Math.floor((this.position / this.hcells))
+        },
+        testColor: function(){
+            
+            return new Color({red:this.tweenedColor.red, green:this.tweenedColor.green, blue:this.tweenedColor.blue, alpha:this.tweenedColor.alpha}).toCSS()
         }
     },
     watch: {
         x: function(val){
             if (this.dragging){
                 if ((Math.floor(val / this.cellSize) == this.position % this.hcells) && (Math.floor(this.y / this.cellSize) == Math.floor(this.position/ this.hcells))){
-                    this.color = "red"
+                    this.color = "black"
                 }
             }
         },
         y: function(val){
             if (this.dragging){
                 if ((Math.floor(this.x / this.cellSize) == this.position % this.hcells) && (Math.floor(val / this.cellSize) == Math.floor(this.position/ this.hcells))){
-                    this.color = "red"
+                    this.color = "black"
                 }
             }
         },
         color: function(val){
+            if (val == 'black'){
             this.$emit('color-change', this.position)
             console.log("color change " + this.position)
-        }
+            }
+        },
+        disappear: function(val){
+            
+            if (val && this.color != null && 'black' === this.color){
+                
+                function animate() {
+                    if (TWEEN.update()){
+                        
+                        requestAnimationFrame(animate)
+                    }
+                }
+                
+                var tween = new TWEEN.Tween(this.tweenedColor)
+                tween.to({red: 1, green: 0.9804, blue: 0.9804, alpha: 1}, 300).start()
+                
+                animate()
+                
+            }
+        },
+        testColor: function(val){
+            
+            this.color = this.testColor
+            
+            
+            
+        },
+        deep: true
         
     },
     template: `
-    
     <div class="cell" v-bind:style="{'background-color': color, left: posX + 'px', top: posY + 'px', width: cellSize + 'px', height: cellSize + 'px'}"></div>
-
     `
 })
-
-
 
 
 
@@ -54,7 +90,8 @@ new Vue({
     data: {
         x: 0,
         y: 0,
-        dragging: false
+        dragging: false,
+        disappear: false
     },
     computed: {
         cellSize: function(){
@@ -92,6 +129,8 @@ new Vue({
 
         getArray(){
             console.log(this.arr)
+            this.disappear = true
+            
         },
 
         incArray: function(position){
