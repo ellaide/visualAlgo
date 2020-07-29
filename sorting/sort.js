2a69d7ba78195c23e4faeb1937e2f4c5bac2d3ba
arnihere <arnur.makenov@gmail.com>
2020-07-30 05:54:29 +0900

    set connection with heroku server, insertion, selection sorts

--- sorting/sort.js
+++ sorting/sort.js
@@ -83,7 +83,7 @@ Vue.component("cell-component", {
 
 
 Vue.component("column-component", {
-    props: ['height', 'colnum','vcells', 'hcells'],
+    props: ['height', 'colnum','vcells', 'hcells', 'moving', 'done'],
     data: function(){
         return {color: null, iscolcaller: true, tweenedColor: {red:1, green: 1, blue: 1, alpha: 1}, signal: false}
     },
@@ -111,7 +111,7 @@ Vue.component("column-component", {
             }
             
             var tween = new TWEEN.Tween(this.tweenedColor)
-            tween.to({red: 0, green: 0, blue: 0, alpha: 1}, 1500).start()
+            tween.to({red: 0.5, green: 0, blue: 1, alpha: 1}, 1500).start()
             
             animate()
         },
@@ -126,7 +126,7 @@ Vue.component("column-component", {
     template: `
     <div>
     <transition-group name="go">
-    <cell-component v-for="n in height" :key="n * height + 10000" :position="startPos-(n-1)*inc" dragging="false" x="-1" y="-1" :hcells="hcells" :vcells="vcells" disappear="false" :columncolor="color">
+    <cell-component  v-for="n in height" :key="n * height + 10000" :position="startPos-(n-1)*inc" dragging="false" x="-1" y="-1" :hcells="hcells" :vcells="vcells" disappear="false" :columncolor="(moving===colnum && !done) ? 'red' : color">
     </cell-component>
     </transition-group>
     </div>
@@ -139,7 +139,7 @@ Vue.component("column-component", {
 
 
 var vm = new Vue({
-    el: '#app',
+    el: '#main',
     data: {
         x: 0,
         y: 0,
@@ -147,7 +147,15 @@ var vm = new Vue({
         disappear: false,
         appear: false,
         pressed: 0,
-        index: 0
+        index: 0,
+        expanded: true,
+        options: [{value: "insertion", text: "Insertion Sort"}, {value: "selection", text: "Selection Sort"}, {value: "quick", text: "Quick Sort"}],
+        selected: "selection",
+        array: "",
+        pairs: [],
+        speed: 1,
+        moving: -1,
+        done: false
     },
     computed: {
         cellSize: function(){
@@ -168,6 +176,9 @@ var vm = new Vue({
                 res.push(i)
             }
             return res
+        },
+        sortingSpeedInMs: function(){
+            return Math.floor(2500/this.speed)
         }
     },
     methods: {
@@ -191,16 +202,61 @@ var vm = new Vue({
             }
         },
 
-        getArray(){
+        async getArray(){
+            if (this.array != ''){
+                const strArr = this.array.toString().split(',')
+                console.log(strArr)
+                for (let i = 0; i < this.arr.length; i++){
+                    this.arr[i] = 0
+                    this.incrementFromInput(i, parseInt(strArr[i]))
+                }
+                
+            }
             console.log(this.arr)
             this.disappear = true
 
             setTimeout(() => {
                 this.appear = true
             }, 2000)
+            this.expanded = false
+            this.array = this.arr.join(",")
+            console.log("array:" + this.array)
+            axios.get("https://visualalgo-server.herokuapp.com/" + this.selected + "/?array=" + this.array)
+            .then(res => {
+                
+                this.pairs = res.data.steps.split("#").map((pair) => {
+                    pair.split("&").map(p => [parseInt(p[0]), parseInt(p[1])])
+                })
+                setTimeout(() => this.startAction(res.data.steps.split("#").map((x)=>x.split("&")).map((y) => [parseInt(y[0]), parseInt(y[1])])), 4000)
+            })
             
         },
-
+        startAction(pairs){
+            let that = this
+            console.log(pairs)
+            function littleLoop(i){
+                setTimeout(() => {
+                    if (i < pairs.length){
+                        let val1 = that.posArr[pairs[i][0]]
+                        let val2 = that.posArr[pairs[i][1]]
+                        that.moving = val2
+                        that.swap(pairs[i][0], val2)
+                        that.swap(pairs[i][1], val1)
+                        console.log(that.posArr)
+                        littleLoop(i+1)
+                        
+                    }else{
+                        that.moving = -1
+                        that.done = true
+                    } 
+                }, that.sortingSpeedInMs)
+            }
+            littleLoop(0)
+            
+        },
+        incrementFromInput: function(pos, val){
+            this.arr[pos] += val
+        },
         incArray: function(position){
             this.arr[position % this.horizontalCells]++;
         },
@@ -208,18 +264,39 @@ var vm = new Vue({
             this.posArr[a] = b
         },
 
-        swap(){
-            this.move(3,19)
+        swap(a, b){
+            this.move(a,b)
             this.$forceUpdate()
         },
 
-        reSwap(){
+        reSwap(a, b){
             
-            this.move(3,1)
+            this.move(b,a)
             this.$forceUpdate()
+        },
+
+        randomize(){
+            for (let i = 0; i < this.arr.length; i++){
+                this.incrementFromInput(i, Math.floor(Math.random() * this.verticalCells))
+            }
+            this.array = this.arr.join(",")
+        }
+    },
+    watch: {
+        expanded: function(val){
+            console.log(val)
+        },
+        speed: function(val){
+            console.log(val)
+        },
+        sortingSpeedInMs: function(val){
+            console.log(val)
         }
     },
     mounted(){
+        this.$refs.playground.style.display = "block"
         window.addEventListener('mouseup', this.stopDrag);
     }
 })
+
+
