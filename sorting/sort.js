edb2085eac853485fea63cb87467e1d75cf01913
arnihere <arnur.makenov@gmail.com>
2020-08-02 06:55:10 +0900

    many things added

--- sorting/sort.js
+++ sorting/sort.js
@@ -1,3 +1,5 @@
+
+
 var Color = net.brehaut.Color
 
 
@@ -83,7 +85,7 @@ Vue.component("cell-component", {
 
 
 Vue.component("column-component", {
-    props: ['height', 'colnum','vcells', 'hcells', 'moving', 'done'],
+    props: ['height', 'colnum','vcells', 'hcells', 'moving', 'done', 'pivot', 'comparing'],
     data: function(){
         return {color: null, iscolcaller: true, tweenedColor: {red:1, green: 1, blue: 1, alpha: 1}, signal: false}
     },
@@ -111,7 +113,7 @@ Vue.component("column-component", {
             }
             
             var tween = new TWEEN.Tween(this.tweenedColor)
-            tween.to({red: 0.5, green: 0, blue: 1, alpha: 1}, 1500).start()
+            tween.to({red: 0, green: 0, blue: 0, alpha: 1}, 1500).start()
             
             animate()
         },
@@ -126,7 +128,7 @@ Vue.component("column-component", {
     template: `
     <div>
     <transition-group name="go">
-    <cell-component  v-for="n in height" :key="n * height + 10000" :position="startPos-(n-1)*inc" dragging="false" x="-1" y="-1" :hcells="hcells" :vcells="vcells" disappear="false" :columncolor="(moving===colnum && !done) ? 'red' : color">
+    <cell-component  v-for="n in height" :key="n * height + 10000" :position="startPos-(n-1)*inc" dragging="false" x="-1" y="-1" :hcells="hcells" :vcells="vcells" disappear="false" :columncolor="!done ? (moving===colnum ? 'red' : (pivot===colnum ? 'orange' : (comparing===colnum ? 'blue' : color))) : color">
     </cell-component>
     </transition-group>
     </div>
@@ -150,19 +152,22 @@ var vm = new Vue({
         index: 0,
         expanded: true,
         options: [{value: "insertion", text: "Insertion Sort"}, {value: "selection", text: "Selection Sort"}, {value: "quick", text: "Quick Sort"}],
-        selected: "selection",
+        selected: "quick",
         array: "",
         pairs: [],
         speed: 1,
         moving: -1,
-        done: false
+        done: true,
+        moreThanOnce: false,
+        pivot: -1,
+        comparing: -1
     },
     computed: {
         cellSize: function(){
             return window.innerWidth / this.horizontalCells;
         },
         horizontalCells: function(){
-            return 20
+            return 15
         },
         verticalCells: function(){
             return Math.floor(window.innerHeight * this.horizontalCells / window.innerWidth)
@@ -183,7 +188,17 @@ var vm = new Vue({
     },
     methods: {
         startDrag(event){
-            
+            if (!this.done) return
+            if (this.moreThanOnce) return
+/*            if (this.moreThanOnce){
+                this.appear = false
+                this.disappear = false
+                for (let i = 0; i < this.arr.length; i++){
+                    this.arr[i] = 0
+                    this.posArr[i] = i
+                }
+                this.array = ''
+            }*/
             this.dragging = true
             this.x = event.clientX
             this.y = event.clientY
@@ -208,8 +223,16 @@ var vm = new Vue({
                 console.log(strArr)
                 for (let i = 0; i < this.arr.length; i++){
                     this.arr[i] = 0
+                    this.posArr[i] = i
                     this.incrementFromInput(i, parseInt(strArr[i]))
+                    if (parseInt(strArr[i]) < 0){
+                        strArr[i] = '0'
+                    }
+                    if (parseInt(strArr[i]) > this.verticalCells){
+                        strArr[i] = this.verticalCells + ""
+                    }
                 }
+                this.array = strArr.join(",")
                 
             }
             console.log(this.arr)
@@ -224,17 +247,28 @@ var vm = new Vue({
             axios.get("https://visualalgo-server.herokuapp.com/" + this.selected + "/?array=" + this.array)
             .then(res => {
                 
-                this.pairs = res.data.steps.split("#").map((pair) => {
-                    pair.split("&").map(p => [parseInt(p[0]), parseInt(p[1])])
-                })
-                setTimeout(() => this.startAction(res.data.steps.split("#").map((x)=>x.split("&")).map((y) => [parseInt(y[0]), parseInt(y[1])])), 4000)
+                if (this.selected === 'quick'){
+                    let steps = res.data.steps.split(';').map((x) => x.split(",")).map(y => y.map(z => z.split("&").map(w => parseInt(w))))
+                    setTimeout(() => this.quick(steps), 4000)
+                }
+                else if (this.selected === 'selection'){
+                    let steps = res.data.steps.split("#").map((x)=>x.split("&")).map((y) => [parseInt(y[0]), parseInt(y[1])])
+                    let highlight = res.data.highlight.split("#").map(x => x.split(",")).map(y => y.map(z => parseInt(z)))
+                    setTimeout(() => this.selection(
+                        steps, 
+                        highlight), 4000)
+                }
+                else{
+                    let steps = res.data.steps.split("#").map((x)=>x.split("&")).map((y) => [parseInt(y[0]), parseInt(y[1])])
+                    setTimeout(() => this.insertion(steps), 4000)
+                }
             })
+            this.done = false
             
         },
-        startAction(pairs){
+        insertion(pairs){
             let that = this
-            console.log(pairs)
-            function littleLoop(i){
+            async function littleLoop(i){
                 setTimeout(() => {
                     if (i < pairs.length){
                         let val1 = that.posArr[pairs[i][0]]
@@ -242,7 +276,6 @@ var vm = new Vue({
                         that.moving = val2
                         that.swap(pairs[i][0], val2)
                         that.swap(pairs[i][1], val1)
-                        console.log(that.posArr)
                         littleLoop(i+1)
                         
                     }else{
@@ -252,9 +285,82 @@ var vm = new Vue({
                 }, that.sortingSpeedInMs)
             }
             littleLoop(0)
+        },
+        selection(pairs, highlight){
+            let that = this
+            highlight = highlight || []
+            
+            async function littleLoop(i){
+                setTimeout(() => {
+                    if (i < pairs.length){
+                        async function helper(){
+                            if (highlight.length == 0) return
+                            for (let k = 0; k < highlight[i].length; k++){
+                                await new Promise(resolve => setTimeout(() => {
+                                    that.comparing = that.posArr[highlight[i][k]]
+                                    resolve()
+                                }, that.sortingSpeedInMs/3))
+                            }
+                        }
+                        let val1 = that.posArr[pairs[i][0]]
+                        let val2 = that.posArr[pairs[i][1]]
+                        that.moving = val1
+                        helper().then(() => {
+                            that.swap(pairs[i][0], val2)
+                            that.swap(pairs[i][1], val1)
+                            littleLoop(i+1)
+                        })
+                        
+                        
+                    }else{
+                        that.comparing = -1
+                        that.moving = -1
+                        that.done = true
+                    } 
+                }, 500)
+            }
+            littleLoop(0)
             
         },
+        quick(steps){
+            console.log(steps)
+            let that = this
+            let x = 0
+            function action(resolve, step){
+                that.pivot = that.posArr[step[step.length-1][1]]
+                function actionLoop(i){
+                    setTimeout(() => {
+                        if (i < step.length){
+                            let val1 = that.posArr[step[i][0]]
+                            let val2 = that.posArr[step[i][1]]
+                            that.moving = val2
+                            that.swap(step[i][0], val2)
+                            that.swap(step[i][1], val1)
+                            actionLoop(i+1)
+                            x++
+                        }else{
+                            resolve()
+                        }
+                    }, that.sortingSpeedInMs/1.5)
+                }
+                actionLoop(0)
+            }
+            async function ultimate(){
+                for (let i = 0; i < steps.length; i++){
+                    await new Promise(resolve => action(resolve, steps[i]))
+                }
+            }
+
+            ultimate().then(() =>{
+                that.done = true
+                that.moving = -1
+                that.pivot = -1
+            })
+
+        },
         incrementFromInput: function(pos, val){
+            if (val < 0) val = 0
+            if (val > this.verticalCells) val = this.verticalCells
             this.arr[pos] += val
         },
         incArray: function(position){
@@ -277,6 +383,8 @@ var vm = new Vue({
 
         randomize(){
             for (let i = 0; i < this.arr.length; i++){
+                this.arr[i] = 0
+                this.posArr[i] = i
                 this.incrementFromInput(i, Math.floor(Math.random() * this.verticalCells))
             }
             this.array = this.arr.join(",")
@@ -291,6 +399,11 @@ var vm = new Vue({
         },
         sortingSpeedInMs: function(val){
             console.log(val)
+        },
+        done: function(val){
+            if (val){
+                this.moreThanOnce = true
+            }
         }
     },
     mounted(){
